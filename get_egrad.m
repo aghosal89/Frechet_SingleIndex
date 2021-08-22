162e81f3a0ad563f7397b07e5de9e2b117516e9a
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2021-08-22 11:56:55 -0700

    Delete get_egrad.m

--- get_egrad.m
+++
@@ -1,8 +0,0 @@
-%% Compute gradient for optimization, using weights w, data Y and input y on manifold M
-
-function eg = get_egrad(w, Y, y, M)
-
-	gi = -2*cell2mat(arrayfun(@(k) w(k)*M.dist(Y(:, k), y)*(1 - (Y(:, k)'*y)^2)^(-1/2)*Y(:, k), 1:size(Y, 2), 'UniformOutput', false)); % element-wise gradient
-	eg = sum(gi')';
-	
-end
