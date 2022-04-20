2a6b1ae305ea51dfd0c6165c3bc740591b9f14df
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2022-04-20 00:06:42 -0700

    Delete get_cost.m

--- get_cost.m
+++
@@ -1,7 +0,0 @@
-%% Compute cost function for optimization, using weights w, data Y and input y on manifold M
-
-function c = get_cost(w, Y, y, M)
-
-	c = dot(w, arrayfun(@(j) (M.dist(y, Y(:, j)))^2, 1:size(Y, 2)));
-	
-end
