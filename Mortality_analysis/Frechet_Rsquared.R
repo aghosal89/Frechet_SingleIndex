596de145745eacec0ee5eab05e01187e12293103
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2022-09-18 19:38:32 -0700

    Update and rename frechet_Rsquared.R to Frechet_Rsquared.R

--- Mortality_analysis/Frechet_Rsquared.R
+++ Mortality_analysis/Frechet_Rsquared.R
@@ -9,7 +9,7 @@
 #        5) model: To specify "GF" or "LF" for Global Frechet or Local Frechet respectively.
 
 
-frechet_Rsquared <- function(Y=NULL, X=NULL, h= NULL, tt=NULL, model=NULL) {
+Frechet_Rsquared <- function(Y=NULL, X=NULL, h= NULL, tt=NULL, model=NULL) {
   
   if(model=="LF" & is.null(h)==TRUE) {
     error("For Local Frechet model a bandwidth required.")
